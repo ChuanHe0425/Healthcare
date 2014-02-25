984ecc2750119e23f568370f7c36186c0d394b3e
Samir Keco <Samir@Samirs-MacBook-Pro.local>
2014-02-24 23:52:46 -0500

    kecos - Added a basic homepage, created a registration page,

--- src/org/roncare/webapp/Login.java
+++ src/org/roncare/webapp/Login.java
@@ -26,7 +26,7 @@ public class Login extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().write("Hello from /Login!");
+		response.getWriter().write("Hello from /Login get!");
 	}
 
 	/**
@@ -34,6 +34,7 @@ public class Login extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		response.getWriter().write("Hello from /Login post!");
 	}
 
 }

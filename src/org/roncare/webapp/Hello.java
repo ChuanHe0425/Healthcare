8a11a28de5993548557adde5805b0449472e7fba
Oleg <oleg529@yahoo.com>
2014-03-15 11:31:41 -0400

    testing remove slash from hello.java

--- src/org/roncare/webapp/Hello.java
+++ src/org/roncare/webapp/Hello.java
@@ -26,7 +26,7 @@ public class Hello extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().write("Hello from /hello!");
+		response.getWriter().write("Hello from hello!");
 	}
 
 	/**

5c829020f88e4fec870e636628c1ea99250c4dcd
cynthia raymond <craymond@bu.edu>
2014-03-31 14:33:12 +0000

    TBLInsuranceCompany.sql edited online with Bitbucket

--- sql/TBLInsuranceCompany.sql
+++ sql/TBLInsuranceCompany.sql
@@ -3,8 +3,6 @@ CREATE TABLE TBLInsuranceCompany (
   'company_name' varchar(256) DEFAULT NULL,
   'city' varchar(256) DEFAULT NULL,
   'state' varchar(2) DEFAULT NULL,
-  'representative_id' int(11) DEFAULT NULL,
+  'representative_name' varchar(256) DEFAULT NULL,
   PRIMARY KEY ('insurance_company_id'),
-  KEY 'fk_rep_id_ins' ('representative_id'),
-  CONSTRAINT 'fk_rep_id_ins' FOREIGN KEY ('representative_id') REFERENCES 'TBLRepresentative' ('representative_id')
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;

0f12b3fb75284e335f26e6aa594193b85128962f
cynthia raymond <craymond@bu.edu>
2014-03-31 14:22:57 +0000

    TBLCustomerType.sql deleted online with Bitbucket

--- sql/TBLCustomerType.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE TBLCustomerType (
-  'customer_type_id' int(11) NOT NULL AUTO_INCREMENT,
-  'customer_type_text' varchar(256) DEFAULT NULL,
-  PRIMARY KEY ('customer_type_id')
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-INSERT INTO TBLCustomerType(customer_type_text)
-VALUES('Individual'),('Family No Children'),('Family One Child'),('Family More Children');

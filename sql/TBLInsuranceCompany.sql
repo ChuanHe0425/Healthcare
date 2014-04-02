6ee2c1824e5835a5ab59329c83b161449058f510
Allison Fitzgerald <fitzgera@bu.edu>
2014-04-02 01:01:10 +0000

    removed stray comma

--- sql/TBLInsuranceCompany.sql
+++ sql/TBLInsuranceCompany.sql
@@ -4,5 +4,5 @@ CREATE TABLE TBLInsuranceCompany (
   'city' varchar(256) DEFAULT NULL,
   'state' varchar(2) DEFAULT NULL,
   'representative_name' varchar(256) DEFAULT NULL,
-  PRIMARY KEY ('insurance_company_id'),
+  PRIMARY KEY ('insurance_company_id')
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;

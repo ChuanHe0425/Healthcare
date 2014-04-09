d0c6e0fcbb2190e8e85efbff9551805e879c520c
Oleg <oleg529@yahoo.com>
2014-04-09 14:07:43 -0400

    generated getters/setters for plan

--- src/org/roncare/dao/pojo/Plan.java
+++ src/org/roncare/dao/pojo/Plan.java
@@ -6,5 +6,30 @@ public class Plan
 	private State state;
 	private String plan;
 	private InsuranceCompany insuranceCompany;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public State getState() {
+		return state;
+	}
+	public void setState(State state) {
+		this.state = state;
+	}
+	public String getPlan() {
+		return plan;
+	}
+	public void setPlan(String plan) {
+		this.plan = plan;
+	}
+	public InsuranceCompany getInsuranceCompany() {
+		return insuranceCompany;
+	}
+	public void setInsuranceCompany(InsuranceCompany insuranceCompany) {
+		this.insuranceCompany = insuranceCompany;
+	}
 
 }
